a59675953fce51fd96d024a1824f2af78fd2f752
psyrjy <Qwer1234!>
2021-04-25 17:14:08 +0100

    setup

--- testEnv.py
+++ testEnv.py
@@ -1,4 +1,6 @@
 import gym
+import numpy as np
+
 
 env = gym.make("MountainCar-v0")
 env.reset()
@@ -12,13 +14,15 @@ d_os_win_size = (env.observation_space.high - env.observation_space.low) / d_os_
 print(d_os_size)
 print(d_os_win_size)
 
+qtable = np.random.uniform(low=-2, high=0, size=(d_os_size + [env.action_space.n]))
+print(qtable)
 
-# done = False
+done = False
 
-# while not done:
-#     action = 2
-#     new_state, reward, done, _ = env.step(action)
-#     print(new_state)
-#     env.render()
+while not done:
+    action = 2
+    new_state, reward, done, _ = env.step(action)
+    print(reward, new_state)
+    env.render()
 
-# env.close()
+env.close()
